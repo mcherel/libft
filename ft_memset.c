89534809a7170e7a619f1fa32404e57bbf630ee4
Maria CHEREL SOROKINA <mcherel-@e2r3p5.clusters.42paris.fr>
2021-11-27 19:14:51 +0100

    memset

---
+++ ft_memset.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcherel- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/27 18:35:09 by mcherel-          #+#    #+#             */
+/*   Updated: 2021/11/27 19:11:53 by mcherel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stddef.h>
+
+void * ft_memset( void * pointer, int value, size_t count )
+{
+	size_t i = 0;
+
+	while ( i < count)
+	{
+		*((char *)pointer + i) = value;//* pour deferencer et cast 
+		i++;
+	}
+	return (0);
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+
+    int array [] = { 54, 85, 20, 63, 21 };
+    size_t size = sizeof( int ) * 5;
+    int length;
+
+    /* Display the initial values */
+    for( length=0; length<5; length++) {
+        printf( "%d ", array[ length ] );
+    }
+    printf( "\n" );
+
+    /* Reset the memory bloc */
+    ft_memset( array, 0, size );
+
+    /* Display the new values */
+    for( length=0; length<5; length++) {
+        printf( "%d ", array[ length ] );
+    }
+    printf( "\n" );
+    
+    return 0;
+}
