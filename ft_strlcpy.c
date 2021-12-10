ad6b99ffd975b5ee624c0f5586d179aaa4df9dfd
Maria CHEREL SOROKINA <mcherel-@e3r11p9.42.fr>
2021-12-10 15:49:03 +0100

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,30 +6,26 @@
 /*   By: mcherel- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 14:05:46 by mcherel-          #+#    #+#             */
-/*   Updated: 2021/12/08 11:26:20 by mcherel-         ###   ########.fr       */
+/*   Updated: 2021/12/10 15:42:10 by mcherel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*copies up to size - 1 characters from the NUL-terminated string src to dst, 
+ * NUL-terminating the result.*/
 #include "libft.h"
 
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t srcl;
-	size_t dstl;
-	size_t i;
 
 	srcl = ft_strlen(src);
-	dstl = ft_strlen(dst);
-	i = 0;
+
+	if (dstsize == 0)
+		return (srcl);
    	
-	while (src[i] != '\0' && 1 < dstsize)
-			{
-				dst[i] = src[i];
-				i++;
-			}
-	//while (src[i] 1= '\0' && 
-	dst[i] = '\0';
-	//if (dstsize == 0)
-	//	return (0);
-		
+	while (*src && --dstsize)
+	{			
+		*dst++ = *src++;
+	}
+	*dst++ = '\0';		
 	return (srcl);
 }
