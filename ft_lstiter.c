6ba119ea90818e33cc625ae87c71f5e8a717368e
Maria CHEREL SOROKINA <mcherel-@e3r2p6.42.fr>
2021-12-22 12:21:51 +0100

    lstiteri

---
+++ ft_lstiter.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcherel- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/22 12:07:51 by mcherel-          #+#    #+#             */
+/*   Updated: 2021/12/22 12:19:02 by mcherel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst != NULL)
+	{
+		(*f)(lst->content);	
+		lst = lst->next;
+	}
+}
+/*iterates the list and applies (*f) function to each content*/
